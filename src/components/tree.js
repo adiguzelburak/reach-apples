7035eb2a19a43c8de0b75bb228548b10b61e1663
unknown <adiguzelburak@icloud.com>
2023-06-17 20:27:07 +0300

    multi click bug fixed.

--- src/components/tree.js
+++ src/components/tree.js
@@ -1,20 +1,27 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addAppleFloor } from "../redux/floor/floorSlice";
 import { deleteAppleTree, updateAppleOnFloor } from "../redux/tree/treeSlice";
 import Apple from "./apple";
+import cn from "../helpers/cn";
 
 export default function Tree() {
   const treeRef = useRef(null);
   const treeList = useSelector((state) => state.tree.treeList);
+  const basketList = useSelector((state) => state.basket.basketList);
   const dispatch = useDispatch();
+  const [isTreeShaked, setIsTreeShaked] = useState(false);
+
+  const claimAgain = basketList.length === 3;
 
   const shakeTree = () => {
+    if (isTreeShaked) return;
+
+    setIsTreeShaked(true);
     treeRef.current.classList.add("tree-shake");
     const shakeTimeout = setTimeout(() => {
       dropTheApples();
     }, 3000);
-
     return () => clearTimeout(shakeTimeout);
   };
 
@@ -46,22 +53,53 @@ export default function Tree() {
     }, 750);
   };
 
+  const reloadPage = () => {
+    window.location.reload();
+  };
+
   return (
     <div>
       <button
-        onClick={shakeTree}
-        className="bg-green-500 text-green-200 px-6 py-1.5 rounded-xl hover:bg-green-800 transition-colors duration-300"
+        disabled={isTreeShaked && !claimAgain}
+        onClick={claimAgain ? reloadPage : shakeTree}
+        className={cn(
+          isTreeShaked && !claimAgain ? "cursor-not-allowed" : "cursor-pointer",
+          "bg-green-500 text-green-200 px-6 py-1.5 rounded-xl hover:bg-green-800 transition-colors duration-300"
+        )}
       >
-        Claim the apples
+        {claimAgain ? (
+          <div className="flex items-center space-x-5">
+            <div>Reload</div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+              />
+            </svg>
+          </div>
+        ) : (
+          "Claim the apples"
+        )}
       </button>
 
-      <div className="relative cursor-pointer" ref={treeRef}>
+      <div className="relative" ref={treeRef}>
         <img
           onClick={shakeTree}
           src="/images/tree.svg"
           alt="Tree"
           id="apple-tree"
-          className="absolute"
+          className={cn(
+            isTreeShaked ? "cursor-not-allowed" : "cursor-pointer",
+            "absolute"
+          )}
         />
         <div className="flex items-center space-x-10 absolute left-[9rem] top-[12rem] ">
           {treeList.map((apple, index) => (
